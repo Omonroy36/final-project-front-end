e606de7c4f1c949d441707bc0af4356d272604ee
freddyloboq <freddyloboq@gmail.com>
2022-02-24 00:08:29 +0000

    create the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
          {/*  <Route path='/' element={}/> */}
         </Routes>
