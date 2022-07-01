d46f81da4f54b6f48dc01f0add6738b58c9b8851
freddyloboq <freddyloboq@gmail.com>
2022-07-01 23:22:25 +0000

    Cambios

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,10 +2,19 @@ import React from "react";
 import Article from "../components/Article";
 
 const Home = () => {
-    return <Article
+    return 
+        <>
+        <Article
         title='Como convertirse en desarrollador'
         text='Un texto muy largo va a ir aqui'
     />
+    <Article
+        title='Como convertirse en desarrollador'
+        text='Un texto muy largo va a ir aqui'
+        subText='Este seria el subtext del artículo'
+    />
+    </>
+
 }
 
 export default Home;
